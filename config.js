89f722564749e24dc23218eeda5f6aed3928a270
Randimasl <117644553+Randimasl@users.noreply.github.com>
2024-11-20 21:45:26 +0530

    MANISHA-MD

--- config.js
+++ config.js
@@ -8,6 +8,7 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "WnYAmShD#yd_vK26iaD21EqW3E2p4gjYDjyOoxulC3HEiPLsvTz4",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/tjhxsh.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "*🎇WELLCOME TO MANISHA-MD BOT🎇*",
+AUTO_VOICE: process.env.AUTO_VOICE || "true",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 };
