4dbeb9e7c572ba296224fa7339c93e78d76faca7
Randimasl <117644553+Randimasl@users.noreply.github.com>
2024-11-26 05:05:16 +0530

    MANISHA-MD

--- config.js
+++ config.js
@@ -6,8 +6,5 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "WnYAmShD#yd_vK26iaD21EqW3E2p4gjYDjyOoxulC3HEiPLsvTz4",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/tjhxsh.jpg",
-ALIVE_MSG: process.env.ALIVE_MSG || "*🎇WELLCOME TO MANISHA-MD BOT🎇*",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-MODE: process.env.MODE || "public",
+MONGODB: process.env.MONGODB || "mongodb://mongo:tmVNIICplUrrmhmGIujHVSeWeIlrbNmU@junction.proxy.rlwy.net:46680"
 };
